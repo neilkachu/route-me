a988b7c2ebebb06a07f26aae1e26e9dc21d9edb3
neilkachu <neil_edwards@me.com>
2011-08-24 15:42:59 +0100

    Revised map sources for CycleStreets iOS
    + increased max Zoom to 18 + swapped to new OCM tile server + added new
    Ordnance Survey  map source

--- MapView/Map/RMOpenStreetMapSource.m
+++ MapView/Map/RMOpenStreetMapSource.m
@@ -1,5 +1,5 @@
 //
-//  OpenStreetMapsSource.m
+//  OpenCycleMapSource.m
 //
 // Copyright (c) 2008-2009, Route-Me Contributors
 // All rights reserved.
@@ -25,15 +25,14 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#import "RMOpenStreetMapSource.h"
+#import "RMOpenCycleMapSource.h"
 
-@implementation RMOpenStreetMapSource
+@implementation RMOpenCycleMapSource
 
 -(id) init
 {       
 	if(self = [super init]) 
 	{
-		//http://wiki.openstreetmap.org/index.php/FAQ#What_is_the_map_scale_for_a_particular_zoom_level_of_the_map.3F 
 		[self setMaxZoom:18];
 		[self setMinZoom:1];
 	}
@@ -45,29 +44,29 @@
 	NSAssert4(((tile.zoom >= self.minZoom) && (tile.zoom <= self.maxZoom)),
 			  @"%@ tried to retrieve tile with zoomLevel %d, outside source's defined range %f to %f", 
 			  self, tile.zoom, self.minZoom, self.maxZoom);
-	return [NSString stringWithFormat:@"http://tile.openstreetmap.org/%d/%d/%d.png", tile.zoom, tile.x, tile.y];
+	return [NSString stringWithFormat:@"http://a.tile.opencyclemap.org/cycle/%d/%d/%d.png", tile.zoom, tile.x, tile.y];
 }
 
 -(NSString*) uniqueTilecacheKey
 {
-	return @"OpenStreetMap";
+	return @"OpenCycleMap";
 }
 
 -(NSString *)shortName
 {
-	return @"Open Street Map";
+	return @"Open Cycle Map";
 }
 -(NSString *)longDescription
 {
-	return @"Open Street Map, the free wiki world map, provides freely usable map data for all parts of the world, under the Creative Commons Attribution-Share Alike 2.0 license.";
+	return @"Open Cycle Map, the free wiki world map, provides freely usable map data for all parts of the world, under the Creative Commons Attribution-Share Alike 2.0 license.";
 }
 -(NSString *)shortAttribution
 {
-	return @"© OpenStreetMap CC-BY-SA";
+	return @"© OpenCycleMap CC-BY-SA";
 }
 -(NSString *)longAttribution
 {
-	return @"Map data © OpenStreetMap, licensed under Creative Commons Share Alike By Attribution.";
+	return @"Map data © OpenCycleMap, licensed under Creative Commons Share Alike By Attribution.";
 }
 
 @end
